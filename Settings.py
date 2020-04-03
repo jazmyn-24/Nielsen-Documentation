23519e46e85a04c353f2777b5756a262e83b9f38
jazmyn-24 <41077331+jazmyn-24@users.noreply.github.com>
2020-04-03 18:26:36 +0530

    Add files via upload

---
+++ Settings.py
@@ -0,0 +1,58 @@
+token = "50dc2d7c-4d13-4ee9-a62e-1a68d2d210a4"
+netbase_url="https://api.netbase.com/cb/insight-api/2/"
+# MONGO_SERVER="13.68.103.86"
+MONGO_SERVER="10.36.106.41"
+# MONGO_PORT=27017
+MONGO_PORT=27018
+# MONGO_USER="root"
+# MONGO_PASS="RootWNS@1990"
+MONGO_DATABASE="EnrichmentDocs"
+
+CATEGORYTHEME_THREAD_COUNT=6
+
+THEME_SUMMARY="ThemeSummary"
+THEME_SUMMARY_AGGREGATION="ThemeSummary_Aggregation"
+THEME_CATEGORY="ThemeCategory"
+THEME_SOURCES="ThemeSources"
+NETBASE_MAPPING="NetbaseMapping"
+NETBASE_MAPPING_BRANDS="NetbaseMapping_Brands"
+THEME_SOURCE="ThemeSource"
+MARKET_MAP="MarketMap"
+BRAND_SOURCE="BrandSource"
+BRAND_SUMMARY="BrandSummary"
+NIELSEN_DATA="NielsenData"
+MINTEL_DATA="MintelData"
+BRANDS="brands"
+ESTABLISHED_BRANDS="EstablishedBrands"
+CLEAN_MINTEL="clean_mintel"
+CLEAN_NIELSEN = "clean_nielsen"
+CLEAN_MINTEL_TEST="clean_mintel_test"
+POST_CLEAN_MINTEL="post_clean_mintel"
+POST_CLEAN_NIELSEN = "post_clean_nielsen"
+BING_SEARCH_MINTEL="bing_search_mintel"
+BING_SEARCH_NIELSEN="bing_search_nielsen"
+AIDLE_DATA="aidle_data"
+MONIT="monit"
+MARKET_XPATH="MarketXpath"
+BRAND_PROFILE="brandprofile"
+NETBASE_SUMMARY="NetbaseSummary"
+TREND_SCORE_METHOD="TrendScoreMethod"
+EXACT_MATCHES="Exact_Matches"
+PARTIAL_MATCHES="Partial_Matches"
+CRUNCHBASE="Crunchbase_Brands"
+HEADQUARTER_INFO="HeadquarterInfo"
+CATEGORY_MAPPING="CategoryMapping"
+
+
+theme_ids="623461,623463,623465,635555"
+access_token="50dc2d7c-4d13-4ee9-a62e-1a68d2d210a4"
+azure_account_name="salrsuse2tcccebtdev0001"
+azure_account_key="t5FeOXUA9Q+oPvziqSRWnZBQEvYbpMTqX/y3oNxEk2LuozPo8iE5UxNF9Kcw6sCj0OnISc8wMD45UAUIXxZnaQ=="
+bing_subscription_key = "e040c8169745470bb25254f99b789364"
+
+""" twitter and facebook tokens """
+consumer_key='a3zVDSqxx3mWleIcBHxxoTZ4f'
+consumer_secret='6YICcq9UwDA27QLTIj5suj4hCgeSiSM6HWXz5hiNKWxd1kEZMD'
+access_token_key='65569416-1UbhGclRmQVtA7VYUf1NZbBgLnbsnZrKIki16hxnS'
+access_token_secret='RORQ5yImEFQ3OqPsi9h448dtHktH6EcPvy1w04dCDYSQ2'
+facebook_token='EAAFU6FbZAyewBAP0xE0SrEgTyGm9jC7hj5EHAW6TbdB1nVWloVOry0RESxJz1O0VtEdTEvIeWidcbKljDrSw5BC4chK9GI9DSI0nfARrtvDTbBwZC5s1Af2ZBU5ylTWwQYplvnRQPqNrzsUpPw4F5HXkxOEQKqbjg7ZCTP8ZBaAZDZD'
